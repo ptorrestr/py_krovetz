05a2ba4a2f2ba0e9ca7bf5ec4db24307a94e8df3
Pablo Torres <pablo.torres@insight-centre.org>
2019-03-06 16:00:06 -0300

    use manylinux docker image to build wheel for linux releases

---
+++ build-manylinux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x -e
+
+# Install anything need (non-python tools) by this package
+# Nothing to install
+
+# Compile wheels
+for PYBIN in /opt/python/cp3[5-7]*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/cp3[5-7]*/bin/; do
+    "${PYBIN}/pip" install krovetz --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest" /io/tests --benchmark-skip)
+done
