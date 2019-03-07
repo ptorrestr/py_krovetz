8111f4f934c078fbfe173e782074eb8e053c02e0
Pablo Torres <pablo.torres@insight-centre.org>
2019-03-07 10:55:09 -0300

    fix ident

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -11,18 +11,18 @@ rm -rf /io/build
 
 # Compile wheels
 for PYBIN in /opt/python/cp3[5-7]*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+	"${PYBIN}/pip" install -r /io/requirements.txt
+	"${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
 	auditwheel show "$whl"
-  auditwheel repair "$whl" -w /io/wheelhouse/
+	auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/cp3[5-7]*/bin/; do
-    "${PYBIN}/pip" install krovetz --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/pytest" /io/tests --benchmark-skip)
+§	"${PYBIN}/pip" install krovetz --no-index -f /io/wheelhouse
+	(cd "$HOME"; "${PYBIN}/pytest" /io/tests --benchmark-skip)
 done
