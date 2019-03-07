2f5e61ad063e21a1ae1dcd0318aa21494f043984
Pablo Torres <pablo.torres@insight-centre.org>
2019-03-07 10:35:44 -0300

    remove build directory before creating wheels

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -5,8 +5,12 @@ set -x -e
 # Install anything need (non-python tools) by this package
 # Nothing to install
 
+# Make sure no build from the system is kept. This can link
+# the wheel to the host libraries.
+rm -rf /io/build
+
 # Compile wheels
-for PYBIN in /opt/python/cp37*/bin; do
+for PYBIN in /opt/python/cp3[5-7]*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -18,7 +22,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/cp37*/bin/; do
+for PYBIN in /opt/python/cp3[5-7]*/bin/; do
     "${PYBIN}/pip" install krovetz --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/pytest" /io/tests --benchmark-skip)
 done
